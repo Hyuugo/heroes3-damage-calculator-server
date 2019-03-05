5221e07ed785ce1219c2da08a15382037fb4b387
Hyuugo <rudnov.d@gmail.com>
2019-03-05 16:19:44 +0500

    Calculation with basic stats, offense, armorer skills. + google metrics

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 
 const path = require('path')
@@ -5,6 +7,10 @@ const path = require('path')
 const compression = require('compression')
 
 const helmet = require('helmet')
+
+const uuidv4 = require('uuid/v4')
+
+const ua = require('universal-analytics')
 
 const PORT = process.env.PORT || 5000
 
@@ -19,7 +25,10 @@ app.use(express.static(path.join(global.__basedir, 'public')))
 app.use(helmet())
 app.use(compression())
 
-app.get("*", (req, res, next) => {
+app.get('*', (req, res, next) => {
+  const visitor = ua(process.env.ANALYTICS_CODE, uuidv4())
+  visitor.pageview('/damagecalculator').send()
+
   res.sendFile(path.join(global.__basedir, 'public/index.html'))
 })
 
